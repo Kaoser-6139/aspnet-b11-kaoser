98bb86478844081eed5251b1f8e71cda37ced124
Imrul Kaoser <kaoser6139@gmail.com>
2025-05-20 18:33:43 +0600

    Modified

---
+++ DevSlkill.Inventory.Web/Inventory.Application/Features/Products/Commands/ProductListModel.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Application.Features.Products.Commands
+{
+    public  class ProductListModel:DataTables
+    {
+        public ProductSearchModel SearchItem { get; set; }
+    }
+}
---
+++ DevSlkill.Inventory.Web/Inventory.Domain/Dtos/ProductSearchDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain.Dtos
+{
+    public  class ProductSearchDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? PriceFrom { get; set; }
+        public int? PriceTo { get; set; }
+    }
+}
